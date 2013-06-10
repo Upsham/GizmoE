49ee8115a3a26609c05d3a1ffdab8580fafb2f06
Upsham <upsham@gmail.com>
2013-06-10 03:13:06 -0400

    Small change

--- Practicum-Github-GizmoE/src/gizmoe/taskdag/IOPair.java
+++ Practicum-Github-GizmoE/src/gizmoe/taskdag/IOPair.java
@@ -1,3 +1,14 @@
+/************************************************************
+ * 
+ * GizmoE Practicum
+ * IOBase class which will contain packages used to store
+ * inputs and outputs in DAG. This is the base class
+ * 
+ * Author: Upsham Dawra(ukd)
+ * Version: 1.0
+ * 
+ * 
+ ************************************************************/
 package gizmoe.taskdag;
 
 public class IOPair {
